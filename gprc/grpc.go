a4310ccc424854f03bb15d4e43d8ac36ca87436b
严礼鸿 <13226728+YANLIHON@user.noreply.gitee.com>
2024-02-28 12:35:36 +0800

    cert

--- gprc/grpc.go
+++ gprc/grpc.go
@@ -3,17 +3,24 @@ package gprc
 import (
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
 )
 
-func ConcentGrpc(port int, fu func(s *grpc.Server)) error {
+func ConcentGrpc(port int, fu func(s *grpc.Server), cert, key string) error {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
+
+	creds, err := credentials.NewServerTLSFromFile(cert, key)
+	if err != nil {
+		log.Fatalf("failed to create credentials: %v", err)
+
+	}
+	s := grpc.NewServer(grpc.Creds(creds))
 	//反射
 	reflection.Register(s)
 	fu(s)
