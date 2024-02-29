05a5c076db1068f0b890828d85dedeaff4be906e
严礼鸿 <13226728+YANLIHON@user.noreply.gitee.com>
2024-02-29 08:43:59 +0800

    nacos

--- nacos/nacos.go
+++ nacos/nacos.go
@@ -2,9 +2,9 @@ package nacos
 
 import (
 	"fmt"
-	"github.com/nacos-group/nacos-sdk-go/clients"
-	"github.com/nacos-group/nacos-sdk-go/common/constant"
-	"github.com/nacos-group/nacos-sdk-go/vo"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
 )
 
 type Nacos struct {
