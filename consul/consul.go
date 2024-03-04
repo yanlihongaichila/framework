56d63cb0b512f9f33bfda249ceac79e77c1d8e03
严礼鸿 <13226728+YANLIHON@user.noreply.gitee.com>
2024-03-04 21:32:30 +0800

    consul

--- consul/consul.go
+++ consul/consul.go
@@ -1,27 +1,85 @@
-package main
+package consul
 
 import (
 	"fmt"
-	"github.com/google/uuid"
-	capi "github.com/hashicorp/consul/api"
+	"github.com/hashicorp/consul/api"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+	"net"
 )
 
-func main() {
-	client, err := capi.NewClient(capi.DefaultConfig())
-	if err != nil {
-		panic(err)
-	}
+//func main() {
+//	client, err := capi.NewClient(capi.DefaultConfig())
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	err = client.Agent().ServiceRegister(&capi.AgentServiceRegistration{
+//		ID:      uuid.NewString(),
+//		Name:    "test",
+//		Tags:    []string{"GRPC"},
+//		Port:    3306,
+//		Address: "127.0.0.1",
+//	})
+//
+//	if err != nil {
+//		fmt.Println(err)
+//		return
+//	}
+//}
 
-	err = client.Agent().ServiceRegister(&capi.AgentServiceRegistration{
-		ID:      uuid.NewString(),
-		Name:    "test",
-		Tags:    []string{"GRPC"},
-		Port:    3306,
-		Address: "127.0.0.1",
-	})
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+// consul 定义一个consul结构体，其内部有一个`*api.Client`字段。
+type consul struct {
+	client *api.Client
 }
+
+// NewConsul 连接至consul服务返回一个consul对象
+func NewConsul(addr string) (*consul, error) {
+	cfg := api.DefaultConfig()
+	cfg.Address = addr
+	c, err := api.NewClient(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &consul{c}, nil
+}
+
+// GetOutboundIP 获取本机的出口IP
+func GetOutboundIP() (net.IP, error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP, nil
+}
+
+// RegisterService 将gRPC服务注册到consul
+func (c *consul) RegisterService(serviceName string, ip string, port int) error {
+	s := grpc.NewServer() // 创建gRPC服务器
+	healthcheck := health.NewServer()
+	healthpb.RegisterHealthServer(s, healthcheck)
+
+	// 健康检查
+	check := &api.AgentServiceCheck{
+		GRPC:     fmt.Sprintf("%s:%d", ip, 8077), // 这里一定是外部可以访问的地址
+		Timeout:  "10s",                          // 超时时间
+		Interval: "10s",                          // 运行检查的频率
+		// 指定时间后自动注销不健康的服务节点
+		// 最小超时时间为1分钟，收获不健康服务的进程每30秒运行一次，因此触发注销的时间可能略长于配置的超时时间。
+		DeregisterCriticalServiceAfter: "20s",
+	}
+	srv := &api.AgentServiceRegistration{
+		ID:      fmt.Sprintf("%s-%s-%d", serviceName, ip, port), // 服务唯一ID
+		Name:    serviceName,                                    // 服务名称
+		Tags:    []string{"q1mi", "hello"},                      // 为服务打标签
+		Address: ip,
+		Port:    port,
+		Check:   check,
+	}
+	return c.client.Agent().ServiceRegister(srv)
+}
+
+//发现
